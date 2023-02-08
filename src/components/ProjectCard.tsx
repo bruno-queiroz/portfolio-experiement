652db986a30d31bfb3ecaf9aca3bbe9d286b37ac
bruno <brunoqueiroz612@gmail.com>
2023-02-08 16:35:48 -0300

    Create ProjectCard component HTML

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProjectCard = () => {
+  return (
+    <article>
+      <img src="" alt="" />
+      <h3>Modern UI/UX Website</h3>
+      <p>A modern IU/UX Portfolio Website</p>
+    </article>
+  );
+};
+
+export default ProjectCard;
