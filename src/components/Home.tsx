128cd97576ba4331fefe5e6df0a1f2fff22e9cb6
bruno <brunoqueiroz612@gmail.com>
2023-02-08 16:12:34 -0300

    Create Home HTML

---
+++ src/components/Home.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import reduxImg from "../assets/images/redux.png";
+import sassImg from "../assets/images/sass.png";
+import flutterImg from "../assets/images/flutter.png";
+import helloImg from "../assets/images/hello.svg";
+
+const Home = () => {
+  return (
+    <main>
+      <div>
+        <div>
+          <h1>
+            <div>
+              <img src="" alt="" />
+              <span>Hello I am</span>
+            </div>
+            <span>Micael</span>
+          </h1>
+        </div>
+        <div>
+          <strong>WEB DEVELOPER</strong>
+          <strong>FREELANCER</strong>
+        </div>
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
