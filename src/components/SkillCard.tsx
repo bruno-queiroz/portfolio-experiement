4b86583d1fb1a18a999936cf4c12130048ac1c7a
bruno <brunoqueiroz612@gmail.com>
2023-02-08 16:22:03 -0300

    Create SkillCard component HTML

---
+++ src/components/SkillCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SkillCard = () => {
+  return (
+    <article>
+      <img src="" alt="" />
+      <h3>React Native Developer</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+        magni dolor perferendis ipsa corrupti! Doloremque magnam nobis, cum
+      </p>
+    </article>
+  );
+};
+
+export default SkillCard;
