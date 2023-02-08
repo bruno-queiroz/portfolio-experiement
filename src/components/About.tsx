1a59184b9ebdff69d3e6cd8f6ba27cc7cf89e6f4
bruno <brunoqueiroz612@gmail.com>
2023-02-08 16:21:19 -0300

    Create About section HTML

---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section>
+      <h2>
+        I know that <span>Good Apps</span> Means <span>Good Business</span>
+      </h2>
+    </section>
+  );
+};
+
+export default About;
