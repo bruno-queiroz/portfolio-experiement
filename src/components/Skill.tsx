f58b5ea5d8b97b4140ece4218995677d14f2c5da
bruno <brunoqueiroz612@gmail.com>
2023-02-09 15:06:55 -0300

    Create Skill Section HTML

---
+++ src/components/Skill.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+const Skill = () => {
+  return (
+    <section>
+      <h2>Skills & Experience</h2>
+
+      <div>
+        <div>
+          <div>
+            <div>
+              <img src="" alt="" />
+            </div>
+            Git
+          </div>
+
+          <div>
+            <div>
+              <img src="" alt="" />
+            </div>
+            React
+          </div>
+          <div>
+            <div>
+              <img src="" alt="" />
+            </div>
+            Node JS
+          </div>
+          <div>
+            <div>
+              <img src="" alt="" />
+            </div>
+            Figma
+          </div>
+          <div>
+            <div>
+              <img src="" alt="" />
+            </div>
+            Flutter
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <div>
+            <span>2020</span>
+          </div>
+          <div>
+            <div>
+              <span>Frontend Developer</span>
+              <span>Google</span>
+            </div>
+            <div>
+              <span>Blockchain</span>
+              <span>Facebook</span>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <span>2021</span>
+          </div>
+          <div>
+            <div>
+              <span>Backend Developer</span>
+              <span>Meta</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skill;
