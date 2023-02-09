d023a3b021c9fed410eaf3379e793c0aecaa9634
bruno <brunoqueiroz612@gmail.com>
2023-02-09 15:23:00 -0300

    Create All HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import About from "./components/About";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Skill from "./components/Skill";
+import TakeACoffe from "./components/TakeACoffeSection";
+import Testimonials from "./components/Testimonials";
+import Work from "./components/Work";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Home />
+      <About />
+      <Work />
+      <Skill />
+      <Testimonials />
+      <TakeACoffe />
+    </>
+  );
 };
 
 export default App;
