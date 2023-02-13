497f64527b506866495311a134a4548e613a1002
bruno <brunoqueiroz612@gmail.com>
2023-02-13 15:48:10 -0300

    Add CSS and SkillCard loop to About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-
+import SkillCard from "./SkillCard";
+import { skillData } from "../../public/portifolioData";
 const About = () => {
   return (
-    <section>
-      <h2>
-        I know that <span>Good Apps</span> Means <span>Good Business</span>
+    <section className="flex flex-col items-center p-4">
+      <h2 className="text-4xl md:text-5xl font-semibold text-center py-12 leading-[4rem]">
+        I know that <span className="text-blue-700">Good Apps</span> Means{" "}
+        <span className="text-blue-700">Good Business</span>
       </h2>
+
+      <div className="grid grid-cols-[repeat(auto-fit,200px)] mt-4 justify-center gap-12 max-w-[1000px] mx-auto w-full pb-12">
+        {skillData.map((skill, index) => (
+          <SkillCard key={index} {...skill} />
+        ))}
+      </div>
     </section>
   );
 };
