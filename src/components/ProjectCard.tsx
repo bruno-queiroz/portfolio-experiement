901e4c1bf580c691ac76744c74945254eb165974
bruno <brunoqueiroz612@gmail.com>
2023-02-14 15:35:18 -0300

    Add CSS to ProjectCard component

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 
-const ProjectCard = () => {
+interface ProjectCardProps {
+  category: string;
+  title: string;
+  img: string;
+  description: string;
+}
+
+const ProjectCard = ({
+  title,
+  category,
+  img,
+  description,
+}: ProjectCardProps) => {
   return (
-    <article>
-      <img src="" alt="" />
-      <h3>Modern UI/UX Website</h3>
-      <p>A modern IU/UX Portfolio Website</p>
+    <article className="flex flex-col gap-3 text-center p-4 bg-white rounded-md shadow-md">
+      <div className="flex relative">
+        <img src={img} alt="" className="object-cover rounded-md" />
+        <div className="absolute bottom-0 left-[50%] translate-x-[-50%] bg-white rounded-t-md px-3 py-1 pb-0 text-gray-500">
+          {category}
+        </div>
+      </div>
+      <h3 className="font-semibold">{title}</h3>
+      <p className="text-gray-500">{description}</p>
     </article>
   );
 };
