aa258bcd9ab6ff49df4aaedbfaaeb1d5f4d51e02
bruno <brunoqueiroz612@gmail.com>
2023-02-14 15:36:06 -0300

    Add CSS and responsiveness to Work Section

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,23 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { projectCard } from "../../public/portifolioData";
 import ProjectCard from "./ProjectCard";
 
 const Work = () => {
+  const [projectCategory, setProjectCategory] = useState("all");
+
+  const changeProjectCategory = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setProjectCategory(event.currentTarget!.textContent as string);
+  };
+
   return (
-    <section>
-      <h2>
-        My Creative <span>Portfolio</span> Section
+    <section className="flex flex-col items-center gap-12 bg-[#EDF2F8] px-4 py-12">
+      <h2 className="text-4xl md:text-5xl font-semibold text-center">
+        My Creative <span className="text-blue-700">Portfolio</span> Section
       </h2>
 
-      <div>
-        <button type="button">UI/UX</button>
-        <button type="button">Web App</button>
-        <button type="button">Mobile App</button>
-        <button type="button">React JS</button>
-        <button type="button">All</button>
+      <div className="flex gap-3 flex-wrap justify-center">
+        <button
+          type="button"
+          className={`p-2 rounded-md font-semibold ${
+            projectCategory === "UI/UX"
+              ? "bg-blue-700 text-white"
+              : "bg-white text-black"
+          }`}
+          onClick={changeProjectCategory}
+        >
+          UI/UX
+        </button>
+        <button
+          type="button"
+          className={`p-2 rounded-md font-semibold ${
+            projectCategory === "Web App"
+              ? "bg-blue-700 text-white"
+              : "bg-white text-black"
+          }`}
+          onClick={changeProjectCategory}
+        >
+          Web App
+        </button>
+        <button
+          type="button"
+          className={`p-2 rounded-md font-semibold ${
+            projectCategory === "Mobile App"
+              ? "bg-blue-700 text-white"
+              : "bg-white text-black"
+          }`}
+          onClick={changeProjectCategory}
+        >
+          Mobile App
+        </button>
+        <button
+          type="button"
+          className={`p-2 rounded-md font-semibold ${
+            projectCategory === "React JS"
+              ? "bg-blue-700 text-white"
+              : "bg-white text-black"
+          }`}
+          onClick={changeProjectCategory}
+        >
+          React JS
+        </button>
+        <button
+          type="button"
+          className={`p-2 rounded-md font-semibold ${
+            projectCategory === "All"
+              ? "bg-blue-700 text-white"
+              : "bg-white text-black"
+          }`}
+          onClick={changeProjectCategory}
+        >
+          All
+        </button>
       </div>
 
-      <div>
-        <ProjectCard />
+      <div className="grid grid-cols-[repeat(auto-fit,300px)] w-full max-w-[1000px] mx-auto justify-center gap-8">
+        {projectCard.map((project, index) => (
+          <ProjectCard key={index} {...project} />
+        ))}
       </div>
     </section>
   );
