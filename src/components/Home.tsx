20aea61bae4e49c84597b592f79ce5297fea225a
bruno <brunoqueiroz612@gmail.com>
2023-02-16 16:43:27 -0300

    Style: rezise the whole section

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,10 @@
-import React from "react";
-import reduxImg from "../assets/images/redux.png";
-import sassImg from "../assets/images/sass.png";
-import flutterImg from "../assets/images/flutter.png";
 import helloImg from "../assets/images/hello.svg";
-import profileImg from "../assets/images/profile.png";
+import profile from "../assets/images/profile.png";
 
 const Home = () => {
   return (
-    <main className="flex flex-col p-4 pt-[100px] bg-[#EDF2F8] xl:grid xl:grid-cols-3 xl:gap-8 max-w-[1440px] mx-auto">
-      <div className="pl-4 sm:pl-[8rem] mt-4">
+    <main className="flex flex-col p-4 pb-0 pt-[150px] bg-[#EDF2F8] xl:grid xl:grid-cols-[1fr_780px_1fr] xl:gap-4 min-h-[100vh] main-background-image">
+      <div className="pl-4 sm:pl-[8rem] xl:pl-4 mt-4 xl:justify-self-end">
         <div className="py-4 px-10 rounded bg-white w-[max-content] shadow-lg">
           <h1 className="flex gap-4 ">
             <img src={helloImg} alt="" className="w-[50px]" />
@@ -23,11 +19,15 @@ const Home = () => {
           <strong className="font-medium text-gray-500">FREELANCER</strong>
         </div>
       </div>
-      <div className="my-8">
-        <div className="background-image" />
+      <div className="mt-8 mb-16 xl:mb-0">
+        <img
+          src={profile}
+          alt=""
+          className="mx-auto lg:w-[750px] mt-[inherit]"
+        />
       </div>
       <div className="flex justify-center mt-4 mx-auto w-[350px] h-[270px] relative sm:w-[60%] xl:w-full xl:h-full ">
-        <div className="flex justify-center items-center w-[90px] h-[90px] rounded-[50%] bg-white shadow-lg absolute top-0 left-0 xl:left-2 ">
+        <div className="flex justify-center items-center w-[110px] h-[110px] rounded-[50%] bg-white shadow-lg absolute top-0 left-0 xl:left-2 xl:top-24 ">
           <div className="w-[50px] h-[50px] rounded-[50%] flutter-bg-image" />
         </div>
         <div className="flex justify-center items-center w-[150px] h-[150px] rounded-[50%] bg-white shadow-lg absolute top-3 right-0 sm:left-[50%] sm:translate-x-[-50%] xl:top-[50%] xl:left-5 xl:sm:translate-x-0 xl:translate-y-[-50%]">
