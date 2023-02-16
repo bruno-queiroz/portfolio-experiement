d3d904943f4960188ae85f1c4d6350e65f14afb4
bruno <brunoqueiroz612@gmail.com>
2023-02-16 15:31:55 -0300

    Add CSS and responsiveness to TakeACoffeSection

--- src/components/TakeACoffeSection.tsx
+++ src/components/TakeACoffeSection.tsx
@@ -1,30 +1,50 @@
 import React from "react";
 
+import emailImg from "../assets/images/email.png";
+import mobileImg from "../assets/images/mobile.png";
+
 const TakeACoffeSection = () => {
   return (
-    <section>
-      <h2>Take A Coffe & Chat With Me</h2>
+    <section className="bg-white py-20 px-4">
+      <div className="max-w-[700px] w-full mx-auto">
+        <h2 className="text-4xl font-semibold text-center mb-24">
+          Take A Coffe & Chat With Me
+        </h2>
 
-      <div>
-        <div>
-          <img src="" alt="" />
-          <a href="tel: +1 (123) 456-789">+1 (123) 456-789</a>
+        <div className="flex flex-col gap-4 items-center text-gray-600">
+          <div className="flex gap-4 items-center py-4 px-6 bg-[#EDF2F8] max-w-[500px] w-full">
+            <img src={mobileImg} alt="" className="w-[40px] h-[40px]" />
+            <a href="tel: +1 (123) 456-789">+1 (123) 456-789</a>
+          </div>
+          <div className="flex gap-4 items-center py-4 px-6 bg-[#EDF2F8] max-w-[500px] w-full">
+            <img src={emailImg} alt="" className="w-[40px] h-[40px]" />
+            <a href="mailto:hello@micael.com">hello@micael.com</a>
+          </div>
         </div>
-        <div>
-          <img src="" alt="" />
-          <a href="mailto:hello@micael.com"></a>
-        </div>
+
+        <form className="flex flex-col gap-4 mt-20">
+          <input
+            type="text"
+            placeholder="Your name"
+            aria-label="your name"
+            className="bg-[#EDF2F8] p-3"
+          />
+          <input
+            type="email"
+            placeholder="Your email"
+            aria-label="your email"
+            className="bg-[#EDF2F8] p-3"
+          />
+          <textarea
+            aria-label="your message"
+            placeholder="Your message"
+            className="bg-[#EDF2F8] p-3"
+          ></textarea>
+          <button className="bg-blue-700 py-3 px-4 rounded text-white mx-auto mt-4 w-[max-content]">
+            Send Message
+          </button>
+        </form>
       </div>
-
-      <form>
-        <input type="text" placeholder="Your name" aria-label="your-name" />
-        <input type="email" placeholder="Your email" aria-label="your-email" />
-        <textarea
-          aria-label="your-message"
-          placeholder="Your message"
-        ></textarea>
-        <button>Send Message</button>
-      </form>
     </section>
   );
 };
