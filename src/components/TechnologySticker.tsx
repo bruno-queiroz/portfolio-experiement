af7eb54ee6ffff8512c9872b8f82ad5a075ebc38
bruno <brunoqueiroz612@gmail.com>
2023-02-17 15:35:26 -0300

    Style: increase sticker size

--- src/components/TechnologySticker.tsx
+++ src/components/TechnologySticker.tsx
@@ -9,7 +9,7 @@ const TechnologySticker = ({ technology, img }: TechnologyStickerProps) => {
   return (
     <div className="flex flex-col gap-3 items-center">
       <div className="w-[max-contet] p-4  rounded-[50%] bg-[#EDF2F8]">
-        <img src={img} alt="" className="object-cover  w-[45px] h-[45px]" />
+        <img src={img} alt="" className="object-cover  w-[55px] h-[55px]" />
       </div>
       {technology}
     </div>
