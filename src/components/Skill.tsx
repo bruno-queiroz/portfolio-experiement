0af8dd41def4a95e3088517e3ac0359421c9126f
bruno <brunoqueiroz612@gmail.com>
2023-02-17 15:35:49 -0300

    Style: change Skill section height

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -6,12 +6,12 @@ import TechnologySticker from "./TechnologySticker";
 
 const Skill = () => {
   return (
-    <section className="flex flex-col gap-4 p-4 py-24">
-      <h2 className="text-4xl text-center font-semibold py-12">
+    <section className="flex flex-col justify-center gap-4 p-4 py-12 min-h-[100vh]">
+      <h2 className="text-4xl md:text-5xl text-center font-semibold py-12">
         Skills & Experience
       </h2>
       <div className="flex flex-col gap-12 lg:grid lg:grid-cols-2 lg:max-w-[800px] lg:mx-auto">
-        <div className="flex gap-4 flex-wrap justify-center w-full">
+        <div className="flex gap-6 flex-wrap justify-center w-full">
           {TechnologiesData.map((technology, index) => (
             <TechnologySticker key={index} {...technology} />
           ))}
