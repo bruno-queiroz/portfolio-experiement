bd2b94cabc969fb857b3aa941d1b26a82930f012
bruno <brunoqueiroz612@gmail.com>
2023-02-17 15:22:20 -0300

    Style: resize About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,10 +3,11 @@ import SkillCard from "./SkillCard";
 import { skillData } from "../../public/portifolioData";
 const About = () => {
   return (
-    <section className="flex flex-col items-center p-4">
-      <h2 className="text-4xl md:text-5xl font-semibold text-center py-12 leading-[4rem]">
-        I know that <span className="text-blue-700">Good Apps</span> Means{" "}
-        <span className="text-blue-700">Good Business</span>
+    <section className="flex flex-col items-center justify-center p-4 min-h-[100vh]">
+      <h2 className="text-4xl max-w-[525px] font-semibold text-center py-12 leading-[3rem] lg:leading-[10rem]]">
+        <span className="">I know that</span>{" "}
+        <span className="text-blue-700">Good Apps</span>
+        <span> Means</span> <span className="text-blue-700">Good Business</span>
       </h2>
 
       <div className="grid grid-cols-[repeat(auto-fit,200px)] mt-4 justify-center gap-12 max-w-[1000px] mx-auto w-full pb-12">
