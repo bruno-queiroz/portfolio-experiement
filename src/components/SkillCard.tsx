da8233b0dc7477557c9d71adbbd34217cc3833ce
bruno <brunoqueiroz612@gmail.com>
2023-02-17 15:21:23 -0300

    Style: change text-color

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -11,7 +11,7 @@ const SkillCard = ({ title, img, description }: SkillCardProps) => {
     <article className="flex flex-col gap-3">
       <img src={img} alt="" className="object-cover rounded-3xl" />
       <h3 className="font-semibold text-lg">{title}</h3>
-      <p>{description}</p>
+      <p className="text-gray-600">{description}</p>
     </article>
   );
 };
