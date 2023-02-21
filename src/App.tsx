167791e0e065feab764b1ca538be71b4001299dd
bruno <brunoqueiroz612@gmail.com>
2023-02-21 15:33:45 -0300

    Change Skills section name

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import Skill from "./components/Skill";
-import TakeACoffe from "./components/TakeACoffeSection";
+import Skill from "./components/Skills";
+import TakeACoffe from "./components/TakeACoffee";
 import Testimonials from "./components/Testimonials";
 import Work from "./components/Work";
 
