a2ff6ddfe28f7f5cb0c88ac5673293510e015d21
bruno <brunoqueiroz612@gmail.com>
2023-02-21 08:20:53 -0300

    Add lazy loading to Home section

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,66 @@
+import { useEffect, useRef, useState } from "react";
 import helloImg from "../assets/images/hello.svg";
 import profile from "../assets/images/profile.png";
+import { createObserver } from "../utils/intersectionObserver";
 
 const Home = () => {
+  const firstLazyLoadingElementRef = useRef<HTMLDivElement>(null);
+  const secondLazyLoadingElementRef = useRef<HTMLDivElement>(null);
+  const thirdLazyLoadingElementRef = useRef<HTMLDivElement>(null);
+
+  const [isIntersectingFirstElement, setIsIntersectingFirstElement] =
+    useState(false);
+  const [isIntersectingSecondElement, setIsIntersectingSecondElement] =
+    useState(false);
+  const [isIntersectingThirdElement, setIsIntersectingThirdElement] =
+    useState(false);
+
+  const handleHomeIntersection = (entry: IntersectionObserverEntry) => {
+    if (entry.isIntersecting) {
+      if (entry.target.id === "first-lazy-loading-element") {
+        setIsIntersectingFirstElement(true);
+        return;
+      }
+      if (entry.target.id === "second-lazy-loading-element") {
+        setIsIntersectingSecondElement(true);
+        return;
+      }
+      setIsIntersectingThirdElement(true);
+      return;
+    }
+    if (entry.target.id === "first-lazy-loading-element") {
+      setIsIntersectingFirstElement(false);
+      return;
+    }
+    if (entry.target.id === "second-lazy-loading-element") {
+      setIsIntersectingSecondElement(false);
+      return;
+    }
+    setIsIntersectingThirdElement(false);
+  };
+
+  useEffect(() => {
+    const clearObservers = createObserver(handleHomeIntersection, [
+      firstLazyLoadingElementRef.current!,
+      secondLazyLoadingElementRef.current!,
+      thirdLazyLoadingElementRef.current!,
+    ]);
+    return () => clearObservers();
+  }, []);
   return (
     <main className="flex flex-col p-4 pb-0 pt-[150px] bg-[#EDF2F8] xl:grid xl:grid-cols-[1fr_auto_1fr] xl:gap-4 min-h-[100vh] main-background-image">
-      <div className="pl-4 sm:pl-[8rem] xl:pl-4 mt-4 xl:justify-self-end">
-        <div className="py-4 px-10 rounded bg-white w-[max-content] shadow-lg">
+      <div
+        className="pl-4 sm:pl-[8rem] xl:pl-4 mt-4 xl:justify-self-end xl:h-[max-content]"
+        id="first-lazy-loading-element"
+        ref={firstLazyLoadingElementRef}
+      >
+        <div
+          className={`py-4 px-10 rounded bg-white w-[max-content] shadow-lg transition-all duration-300 ${
+            isIntersectingFirstElement
+              ? "translate-x-0 opacity-100"
+              : "translate-x-[-100px] bg-transparent opacity-0"
+          } `}
+        >
           <h1 className="flex gap-4 ">
             <img src={helloImg} alt="" className="w-[50px]" />
             <div className="flex gap-2 flex-col">
@@ -14,19 +69,37 @@ const Home = () => {
             </div>
           </h1>
         </div>
-        <div className="py-4 px-6 bg-white rounded flex flex-col gap-2 w-[max-content] mt-8">
+        <div
+          className={`py-4 px-6 bg-white rounded flex flex-col gap-2 w-[max-content] mt-8 transition-all duration-500 ${
+            isIntersectingFirstElement
+              ? "translate-x-0 opacity-100"
+              : "translate-x-[-100px] opacity-0"
+          }`}
+        >
           <strong className="font-medium text-gray-500">WEB DEVELOPER</strong>
           <strong className="font-medium text-gray-500">FREELANCER</strong>
         </div>
       </div>
-      <div className="mt-8 mb-16 xl:mb-0">
+      <div className="mt-8 mb-16 xl:mb-0 relative">
+        <div
+          className={`absolute w-full max-w-[566px] aspect-square rounded-[50%] bg-gradient-to-r from-[#EDF2F8] to-gray-300 z-[1] left-[50%] top-[50%] translate-x-[-50%] translate-y-[-40%] transition-transform duration-500 ${
+            isIntersectingSecondElement ? "scale-100" : "scale-0"
+          }`}
+          id="second-lazy-loading-element"
+          ref={secondLazyLoadingElementRef}
+        />
         <img
           src={profile}
           alt=""
-          className="mx-auto lg:w-[750px] mt-[inherit]"
+          className="mx-auto lg:w-[750px] mt-[inherit] relative z-10"
         />
       </div>
-      <div className="flex justify-center mt-4 mx-auto w-[350px] h-[270px] relative sm:w-[60%] xl:w-full xl:h-full ">
+      <div
+        className={`flex justify-center mt-4 mx-auto w-[350px] h-[270px] relative sm:w-[60%] xl:w-full xl:h-full transition-transform duration-500 ${
+          isIntersectingThirdElement ? "scale-100" : "scale-0"
+        }`}
+        ref={thirdLazyLoadingElementRef}
+      >
         <div className="flex justify-center items-center w-[110px] h-[110px] rounded-[50%] bg-white shadow-lg absolute top-0 left-0 xl:left-2 xl:top-24 ">
           <div className="w-[50px] h-[50px] rounded-[50%] flutter-bg-image" />
         </div>
