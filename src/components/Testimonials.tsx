950603a77a76bbe568119c768f1a974511aaa56c
bruno <brunoqueiroz612@gmail.com>
2023-02-21 15:27:36 -0300

    Add lazy loading to Testimonials section

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { testimonialsBrandsImg } from "../../public/portifolioData";
+import { createObserver } from "../utils/intersectionObserver";
 import Carousel from "./Carousel";
 
 const Testimonials = () => {
+  const sectionElementRef = useRef<HTMLDivElement>(null);
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  const handleTestimonialsIntersection = (entry: IntersectionObserverEntry) => {
+    setIsIntersecting(entry.isIntersecting);
+  };
+
+  useEffect(() => {
+    const clearIntersection = createObserver(
+      handleTestimonialsIntersection,
+      [sectionElementRef.current!],
+      "-100px"
+    );
+    return () => {
+      clearIntersection();
+    };
+  }, []);
   return (
-    <section className="flex flex-col items-center justify-center bg-[#EDF2F8] min-h-[100vh]">
+    <section
+      className={`flex flex-col items-center justify-center bg-[#EDF2F8] min-h-[100vh] transition-all duration-700 ${
+        isIntersecting
+          ? "translate-y-0 opacity-1"
+          : "translate-y-[80px] opacity-0"
+      }`}
+      ref={sectionElementRef}
+    >
       <div className="p-4 py-12 flex flex-col gap-12">
         <Carousel />
         <div className="grid grid-cols-[repeat(auto-fit,170px)] justify-center gap-4">
