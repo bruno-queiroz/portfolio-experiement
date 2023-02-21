6b7a8ead9bfb98155f7dc92570e9464a18afe66f
bruno <brunoqueiroz612@gmail.com>
2023-02-21 15:20:27 -0300

    Add lazy loading to Skill section

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import { experienceData, TechnologiesData } from "../../public/portifolioData";
+import { createObserver } from "../utils/intersectionObserver";
 import CareerTimeline from "./CareerTimeline";
 import TechnologySticker from "./TechnologySticker";
 
 const Skill = () => {
+  const sectionElementRef = useRef<HTMLDivElement>(null);
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  const handleSkillIntersection = (entry: IntersectionObserverEntry) => {
+    setIsIntersecting(entry.isIntersecting);
+  };
+
+  useEffect(() => {
+    const clearIntersection = createObserver(
+      handleSkillIntersection,
+      [sectionElementRef.current!],
+      "-100px"
+    );
+    return () => {
+      clearIntersection();
+    };
+  }, []);
   return (
-    <section className="flex flex-col justify-center gap-4 p-4 py-12 min-h-[100vh]">
+    <section
+      className={`flex flex-col justify-center gap-4 p-4 py-12 min-h-[100vh] transition-all duration-700 ${
+        isIntersecting
+          ? "translate-y-0 opacity-1"
+          : "translate-y-[80px] opacity-0"
+      }`}
+      ref={sectionElementRef}
+    >
       <h2 className="text-4xl md:text-5xl text-center font-semibold py-12">
         Skills & Experience
       </h2>
