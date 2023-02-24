b932436271ac1c482cbb376ab53a7805f7eb6769
bruno <brunoqueiroz612@gmail.com>
2023-02-24 08:04:52 -0300

    Add imageUrlBuilder to SkillCard component

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,15 +1,24 @@
+import ImageUrlBuilder from "@sanity/image-url";
 import React from "react";
+import { Skill } from "./About";
 
-interface SkillCardProps {
-  title: string;
-  description: string;
-  img: string;
-}
+const SkillCard = ({ title, image, description }: Skill) => {
+  const builder = ImageUrlBuilder({
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    dataset: "production",
+  });
 
-const SkillCard = ({ title, img, description }: SkillCardProps) => {
+  const urlFor = (source: string) => {
+    return builder.image(source);
+  };
+
   return (
     <article className="flex flex-col gap-3">
-      <img src={img} alt="" className="object-cover rounded-3xl" />
+      <img
+        src={urlFor(image?.asset?._ref).url()}
+        alt=""
+        className="object-cover rounded-3xl"
+      />
       <h3 className="font-semibold text-lg">{title}</h3>
       <p className="text-gray-600">{description}</p>
     </article>
